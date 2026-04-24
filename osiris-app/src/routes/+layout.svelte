ee425eb350d2ca291cecc663b4fa80f54bb36a1f
Eduardo Prates Tiadoro <66331484+Eduardo-Prates@users.noreply.github.com>
2026-04-24 17:44:31 -0300

    login/singup with oauth supabase

--- osiris-app/src/routes/+layout.svelte
+++ osiris-app/src/routes/+layout.svelte
@@ -1,8 +1,26 @@
 <script>
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { supabase } from '$lib/supabase';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	let { data, children } = $props();
+
+	// Usamos $state para que qualquer componente que use a sessão seja atualizado
+	let session = $state(data.session);
+
+	onMount(() => {
+		const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, _session) => {
+			// Se o token mudar, invalidamos os dados para o load() rodar novamente
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+			session = _session;
+		});
+
+		return () => subscription.unsubscribe();
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
